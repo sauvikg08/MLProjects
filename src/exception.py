e2c7320e0c7301bff8c00c6dea6cdfc87c434199
Sauvik Goswami <sauvikg08@gmail.com>
2024-12-25 22:00:58 +0530

    Logging and Exception Handling - removed errors

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,7 +17,6 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message=error_message_details(error_message, error_detail=error_detail)
     
-    def __tr__(self):
+    def __str__(self):
         return self.error_message
 
-    
